756f8c984584ae571da0a7e37184b1b6dd6fd70e
mazzz3r <i@mazzz3r.ru>
2022-08-01 00:02:20 +0300

    fix webhook setup error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from app.bot import bp as bot_blueprint, create_webhook
+from app.bot import bp as bot_blueprint
 from app.logger import bp as logger_blueprint
 
 app = Flask(
@@ -11,6 +11,5 @@ app = Flask(
 
 app.register_blueprint(logger_blueprint, url_prefix='/logger')
 app.register_blueprint(bot_blueprint, url_prefix='/bot')
-create_webhook()
 
 #  app.run()
