eb99c6f740b8d79605e1fddd1c82bd15f077407d
mazzz3r <i@mazzz3r.ru>
2022-08-01 15:27:19 +0300

    spicify regex rule for flask

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.bot import bp as bot_blueprint
 from app.logger import bp as logger_blueprint
 from app.database import Base, engine
 from app.database import models
+from app.logger.utilities import RegexConverter
 
 
 Base.metadata.create_all(engine)
@@ -14,6 +15,7 @@ app = Flask(
     static_url_path="/static"
 )
 
+app.url_map.converters['regex'] = RegexConverter
 app.register_blueprint(logger_blueprint, url_prefix='/')
 app.register_blueprint(bot_blueprint, url_prefix='/bot')
 
